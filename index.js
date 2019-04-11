6e5d3148928102f70e9b09833eb953c0cd9880f4
unknown <you@example.com>
2019-04-11 17:32:50 +0530

    first intro for es6

---
+++ index.js
@@ -0,0 +1,21 @@
+let word1 = "Lahiru";
+let word2 = "Amarathunga";
+
+let num1 = 2;
+let num2 = 3;
+
+//const fullName = word1 + " " + word2;           //using old method
+
+//Concatenation
+const fullName = `My Name is ${word1} ${word2}`;
+
+//Add numbers
+const total = `${num1 + num2}`;
+
+//Multi Line strings
+const twoLines = `${word1} 
+${word2}`;
+
+console.log(fullName);
+console.log(total);
+console.log(twoLines);
