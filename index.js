14623f25f0b313b9c7a41b91a2ba6428179510a3
unknown <you@example.com>
2019-04-12 17:56:36 +0530

    pass values and print all

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-/* let example1 = [1, 2, 3, 4, 5];
-let example2 = [...example1];
+function add(...nums) {
+  console.log(nums);
+}
 
-console.log(example2);
-
-example2.push(6);
-
-console.log(example2); */
-
-let example1 = {
-  firstName: "Lahiru"
-};
-
-let example2 = {
-  ...example1,
-  lastName: "Amarathunga"
-};
-
-console.log(example2.firstName);
+add(1, 2, 3, 4, 5);
