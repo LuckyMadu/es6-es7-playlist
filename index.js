6ca089a84b5e91cefcce9db0ae917f51fb6c7632
unknown <you@example.com>
2019-04-12 13:10:41 +0530

    cant update values

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ for (const income of incomes) {
   total += income;
 }
 
-console.log(total); */
+console.log(total); 
 
 let fullName = "Lahiru";
 
 for (const char of fullName) {
   console.log(char);
 }
+*/
+
+//not update the values
+let incomes = [10, 20, 30];
+
+for (let income of incomes) {
+  income += 50;
+}
+
+console.log(incomes);
